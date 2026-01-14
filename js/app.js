8de65a75557d6a8145e19573710b680471937861
surv-cel <rnccjatim@gmail.com>
2026-01-14 14:15:08 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -100,7 +100,7 @@ function renderData(data) {
 	   const summary = row["SUMMARY"] || "";
 	   const update  = row["WORKLOG SUMMARY"] || "-";
 	   const zone    = (row["WORKZONE"] || "").toUpperCase();
-       total++; 
+      total++; 
 
 	  if (summary.includes("(REPAIR) TRA T3") || summary.includes("(RECOVERY) TRA T3")) {
 		bb++;
@@ -138,8 +138,8 @@ function renderData(data) {
 	document.getElementById("gponCount").textContent = `GPON : ${g}`;
 	document.getElementById("feederCount").textContent = `FEEDER : ${f}`;
 	document.getElementById("distriCount").textContent = `DISTRIBUSI : ${d}`;
-	document.getElementById("odcCount").textContent = `ODC : ${odc}`;
-	document.getElementById("odpCount").textContent = `ODP : ${odp}`;
+	// document.getElementById("odcCount").textContent = `ODC : ${odc}`;
+	// document.getElementById("odpCount").textContent = `ODP : ${odp}`;
 
 
   if (!jatimBox.children.length) jatimBox.innerHTML = `<div class="empty">Data tidak ditemukan</div>`;
